18ccc7b7fff482b32b74da993eb16a48b002a3bf
felipesouza16 <fesouza2345@gmail.com>
2021-07-13 10:29:28 -0300

    Repository in console app

--- Ex1/MenuOpcoes.cs
+++ Ex1/MenuOpcoes.cs
@@ -38,35 +38,8 @@ namespace Class1
                         Console.WriteLine("Saindo..");
                         break;
                     case 1:
-                        string resposta = "";
-                        List<Usuario> usuarios= new List<Usuario>();
-                        do
-                        {
-                            Console.WriteLine("Digite seu nome: ");
-                            string nome = Validacao.ValidaString();
-                            nome = Char.ToUpper(nome[0]) + nome.Substring(1);
-                            Console.WriteLine("Digite seu sobrenome: ");
-                            string sobrenome = Validacao.ValidaString();
-                            sobrenome = char.ToUpper(sobrenome[0]) + sobrenome.Substring(1);
-                            Console.WriteLine($"{nome} {sobrenome}, digite sua idade: ");
-                            int idade = Validacao.ValidaInt();
-                            Console.WriteLine($"{nome} {sobrenome}, digite seu e-mail: ");
-                            string email = Console.ReadLine();
-                            Console.WriteLine("Digite seu endereço: ");
-                            string endereco = Console.ReadLine();
-                            Usuario user = new Usuario(nome, sobrenome, idade, email, endereco);
-                            usuarios.Add(user);
-                            Console.WriteLine("Deseja cadastrar mais 1? y/n");
-                            resposta = Console.ReadLine();
-                        } while (resposta == "y");
-
-                        //Usuario userSelect1 = usuarios.FirstOrDefault(u => u.nome.Equals("joelma"));
-                        //Usuario userSelect2 = (from u in usuarios where u.nome.Equals ("joelma") select u).First(); 
-
-                        foreach (var item in usuarios)
-                        {
-                            Console.WriteLine($" Nome: {item.Nome} {item.Sobrenome}\n Idade: {item.Idade}  \n E-mail: {item.Email}\n Endereço: {item.Endereco}\n");
-                        }
+                        ModuloUsuario modulo = new ModuloUsuario();
+                        modulo.Create();
                         break;
                     case 2:
                         Console.WriteLine("EM MANUTENÇÃO");
---
+++ Ex1/ModuloUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Class1
+
+{
+    class ModuloUsuario
+    {
+        UsuarioRepository repository = new UsuarioRepository();
+        public void Create()
+        {
+            string change = "";
+            do
+            {
+                Console.WriteLine("Digite seu nome: ");
+                string nome = Validacao.ValidaString();
+                nome = Char.ToUpper(nome[0]) + nome.Substring(1);
+                Console.WriteLine("Digite seu sobrenome: ");
+                string sobrenome = Validacao.ValidaString();
+                sobrenome = char.ToUpper(sobrenome[0]) + sobrenome.Substring(1);
+                Console.WriteLine($"{nome} {sobrenome}, digite sua idade: ");
+                int idade = Validacao.ValidaInt();
+                Console.WriteLine($"{nome} {sobrenome}, digite seu e-mail: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite seu endereço: ");
+                string endereco = Console.ReadLine();
+
+                Usuario model = new Usuario(nome, sobrenome, idade, email, endereco);
+                repository.Create(model);       
+                Console.WriteLine("Deseja cadastrar mais 1? y/n");
+                change = Console.ReadLine();
+            } while (change == "y");
+                
+        }
+    }
+}
